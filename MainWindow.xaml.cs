31ea0819b6a3d76e71d77923f0026ffe6814eaa4
kryo1337 <kryo1337@gmail.com>
2023-05-02 23:05:32 +0100

    files handling

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,45 @@ namespace LinesCounter
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private List<string> files = new List<string>();
+
+        private void addButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Multiselect = true;
+            dialog.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*";
+
+            if (dialog.ShowDialog() == true)
+            {
+                foreach (string file in dialog.FileNames)
+                {
+                    files.Add(file);
+                    fileList.Items.Add(file);
+                }
+            }
+        }
+
+        private void clearButton_Click(object sender, RoutedEventArgs e)
+        {
+            files.Clear();
+            fileList.Items.Clear();
+            outputBox.Clear();
+        }
+
+        private void startButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void deleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            while (fileList.SelectedItems.Count > 0)
+            {
+                files.Remove((string)fileList.SelectedItems[0]);
+                fileList.Items.Remove(fileList.SelectedItems[0]);
+            }
         }
     }
 }
